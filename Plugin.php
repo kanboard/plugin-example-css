273c408d24083b9d2eebfbea968f1d362ce941fd
Atul Kapoor <atulkapoor@live.com>
2016-10-20 17:33:41 -0400

    Update template hook parameter to array and add metadata

--- Plugin.php
+++ Plugin.php
@@ -8,6 +8,31 @@ class Plugin extends Base
 {
     public function initialize()
     {
-        $this->hook->on('template:layout:css', 'plugins/Css/skin.css');
+        $this->hook->on("template:layout:css", array("template" => "plugins/Css/skin.css"));
+    }
+
+    public function getPluginName()
+    {
+        return 'Css';
+    }
+
+    public function getPluginDescription()
+    {
+        return t('This plugin add a new stylesheet and override default styles.');
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'Author';
+    }
+
+    public function getPluginVersion()
+    {
+        return '0.0.1';
+    }
+
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/kanboard/plugin-example-css';
     }
 }
